c807d264321ac56fb576ef006ebdf85eac8bfec1
KUNQI WAN <56388431+kunqiwan@users.noreply.github.com>
2023-03-05 00:23:46 +0100

    finish render cache
    base on base layout template to render multiple html, improve efficiency

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,28 +1,54 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"path/filepath"
 )
 
+// RenderTemplate renders a template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
-	err := parsedTemplate.Execute(w, nil)
+	//create template cache
+	fmt.Println("this is pass 5")
+	tc, err := createTemplateCache()
 	if err != nil {
-		fmt.Println("error parsing template:", err)
+		log.Fatal(err)
 	}
+	//get requested template from the cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
+	}
+	//create buffer to capture the output of the executed template
+	fmt.Println("this is pass 6")
+	buf := new(bytes.Buffer)
+	err = t.Execute(buf, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("this is pass 77")
+	//render the template
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("this is pass 8")
 }
+
 func createTemplateCache() (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 	//get all files which named *.page.tmpl from ./templates
 	//files.Glob will base on the filepath,find the file that has specific name, it will return a slice
-	pages, err := filepath.Glob("./templates/*.pages.tmpl")
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
 	if err != nil {
 		return myCache, err
 	}
 	//range through all files ending with*.page.tmpl
+	fmt.Println("this is pass 1")
+	fmt.Println("pages:", pages)
 	for _, page := range pages {
 		//Base function will return the page name after ./templates/
 		name := filepath.Base(page)
@@ -32,18 +58,21 @@ func createTemplateCache() (map[string]*template.Template, error) {
 			return myCache, err
 		}
 		//find the layout file
-		matches, err := filepath.Glob("./templates/base.layout.tmpl")
+		fmt.Println("this is pass 2")
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
 		if err != nil {
 			return myCache, err
 		}
 		if len(matches) > 0 {
 			//parse all the base.layout.tmpl files and add them to the template set
-			ts, err = ts.ParseGlob("./templates/base.layout.tmpl")
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
 			if err != nil {
 				return myCache, err
 			}
 		}
 		myCache[name] = ts
+		fmt.Println("this is pass 3")
 	}
+	fmt.Println("this is pass 4")
 	return myCache, nil
 }
